a9ab594bf1c0d8dfa666aaa073ee141351a419f1
Khyosunny <71235165+Khyosunny@users.noreply.github.com>
2020-09-14 07:18:09 +0900

    Add files via upload

---
+++ precache-manifest.27711b3edf2150a33d314c34043a8724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e70642207c7cf0d019eb5d28f09fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8394235912d91bb3d640",
+    "url": "/static/css/main.17ad1714.chunk.css"
+  },
+  {
+    "revision": "e3169f5d3888c4e5c6c0",
+    "url": "/static/js/2.f227d04f.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/static/js/2.f227d04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8394235912d91bb3d640",
+    "url": "/static/js/main.f48145e3.chunk.js"
+  },
+  {
+    "revision": "ba1a55bb526de96fcc0e",
+    "url": "/static/js/runtime-main.c424cbdb.js"
+  }
+]);
